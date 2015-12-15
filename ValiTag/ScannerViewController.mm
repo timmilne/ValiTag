13f3defa1de0e962576a228b41778da46e043a41
TimMilne <tim.milne@target.com>
2015-12-15 17:04:58 -0600

    Minor edit

--- ValiTag/ScannerViewController.mm
+++ ValiTag/ScannerViewController.mm
@@ -1084,8 +1084,8 @@ for (UgiTag *tag in [Ugi singleton].activeInventory.tags) {
         // Terminate sesssion
         [_rfidSdkApi srfidTerminateCommunicationSession:_zebraReaderID];
         _zebraReaderID = -1;
-        _rfidLbl.backgroundColor = UIColorFromRGB(0xCC0000);
         _rfidLbl.text = @"RFID: Zebra connection failed";
+        _rfidLbl.backgroundColor = UIColorFromRGB(0xCC0000);
     }
 }
 
